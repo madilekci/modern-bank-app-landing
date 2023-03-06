80f1c22a7e3f8f3b529d13e40efbdaecb67f7196
Muhammed Ali <36687970+madilekci@users.noreply.github.com>
2023-03-06 12:08:22 +0300

    Implement Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+	const [toggle, setToggle] = useState(false);
+	return (
+		<nav className="w-full flex py-6 justify-between items-center navbar">
+			<img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+			<ul className="list-none sm:flex hidden justify-end items-center flex-1 gap-10">
+				{navLinks.map((navLink) => (
+					<li
+						key={navLink.id}
+						className={`font-poppins font-normal cursor-pointer text-[16px] text-white`}
+					>
+						<a href={`#${navLink.id}`}>{navLink.title}</a>
+					</li>
+				))}
+			</ul>
 
-export default Navbar
+			<div className="sm:hidden flex flex-1 justify-end items-center">
+				<img
+					src={toggle ? close : menu}
+					alt="menu"
+					className="w-[28px] h-[28px] object-contain"
+					onClick={() => {
+						setToggle((prevToggle) => !prevToggle);
+					}}
+				/>
+
+				<div
+					className={`${
+						toggle ? "flex" : "hidden"
+					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+				>
+					<ul className="list-none flex flex-col justify-end items-center flex-1 gap-10">
+						{navLinks.map((navLink) => (
+							<li
+								key={navLink.id}
+								className={`font-poppins font-normal cursor-pointer text-[16px] text-white`}
+							>
+								<a href={`#${navLink.id}`}>{navLink.title}</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
