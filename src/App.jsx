e14d444c8c959bf49a8004c3722921420df57944
Muhammed Ali <36687970+madilekci@users.noreply.github.com>
2023-03-06 11:17:05 +0300

    Add basic landing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import styles from "./style";
 
-const Hello = () => {
-  return (
-    <div>Hello</div>
-  )
-}
+const Hello = () => (
+	<div className="bg-primary w-full overflow-hidden">
 
-export default Hello
+		<div className={`${styles.paddingX} ${styles.flexCenter}`}>
+			<div className={`${styles.boxWidth}`}>Navbar</div>
+		</div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+			<div className={`${styles.boxWidth}`}>Hero</div>
+		</div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+			<div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Clients
+        CTA
+        Footer
+      </div>
+		</div>
+
+	</div>
+);
+
+export default Hello;
