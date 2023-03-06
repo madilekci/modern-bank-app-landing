c697e5f7408feff156de971cd39f21277852cc57
Muhammed Ali <36687970+madilekci@users.noreply.github.com>
2023-03-06 12:17:47 +0300

    add eslint

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import { close, logo, menu } from "../assets";
+import { close, logo, menu } from '../assets';
 
-import { navLinks } from "../constants";
+import { navLinks } from '../constants';
 
 const Navbar = () => {
 	const [toggle, setToggle] = useState(false);
 	return (
-		<nav className="w-full flex py-6 justify-between items-center navbar">
-			<img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
-			<ul className="list-none sm:flex hidden justify-end items-center flex-1 gap-10">
+		<nav className='w-full flex py-6 justify-between items-center navbar'>
+			<img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
+			<ul className='list-none sm:flex hidden justify-end items-center flex-1 gap-10'>
 				{navLinks.map((navLink) => (
 					<li
 						key={navLink.id}
@@ -20,11 +20,11 @@ const Navbar = () => {
 				))}
 			</ul>
 
-			<div className="sm:hidden flex flex-1 justify-end items-center">
+			<div className='sm:hidden flex flex-1 justify-end items-center'>
 				<img
 					src={toggle ? close : menu}
-					alt="menu"
-					className="w-[28px] h-[28px] object-contain"
+					alt='menu'
+					className='w-[28px] h-[28px] object-contain'
 					onClick={() => {
 						setToggle((prevToggle) => !prevToggle);
 					}}
@@ -32,10 +32,10 @@ const Navbar = () => {
 
 				<div
 					className={`${
-						toggle ? "flex" : "hidden"
+						toggle ? 'flex' : 'hidden'
 					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
 				>
-					<ul className="list-none flex flex-col justify-end items-center flex-1 gap-10">
+					<ul className='list-none flex flex-col justify-end items-center flex-1 gap-10'>
 						{navLinks.map((navLink) => (
 							<li
 								key={navLink.id}
