ba25492e6eaa8e8dd8c6effdfea2a5b3c11d9791
Muhammed Ali <madilekci@gmail.com>
2023-03-07 15:24:37 +0300

    Implement business component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({styles}) => (
+  <button type='button' className={`${styles} py-4 px-6 bg-blue-gradient font-poppins font-medium text-[16px] text-primary outline-none`}>
+    Get Started
+  </button>
+)
 
 export default Button
